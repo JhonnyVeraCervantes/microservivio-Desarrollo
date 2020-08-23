cd93aa821efedee396b227182ae8df6fdc9a1533
JhonnyVeraCervantes <45070851+JhonnyVeraCervantes@users.noreply.github.com>
2020-08-22 22:53:00 -0500

    initial commit

---
+++ src/main/java/edu/puce/web/resources/WebRestApplication.java
@@ -0,0 +1,11 @@
+package edu.puce.web.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class WebRestApplication extends Application {
+}
